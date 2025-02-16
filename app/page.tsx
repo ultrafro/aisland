9a7e0ce8a4e4f7265e67bf83fda59b5464bf03c3
ultrafro <hisham.bedri@gmail.com>
2025-02-15 21:58:24 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ import {
   uploadToGCS,
 } from "./requestFunctions";
 import { nanoid } from "nanoid";
-import { get } from "http";
 
 export default function Home() {
   // State for uploaded image
